be604aab4f2190ac2885d2623e275140765df48a
KhalanderCSE734 <khalandermohammed734@gmail.com>
2026-01-31 22:46:22 +0530

    Inheritence, Factories & Interaction

--- Basics/master_solidity/examples/7_Secret.sol
+++ Basics/master_solidity/examples/7_Secret.sol
@@ -32,12 +32,47 @@ contract Secret is Ownable {
     address secretVault;
 
     constructor(string memory _secret) {
-        SecretVault _secretVault = new SecretVault(_secret);
+        SecretVault _secretVault = new SecretVault(_secret);        // This is like creating new CONTRACT dynamically, (Factories)
         secretVault = address(_secretVault);
         super;
     }
 
     function getSecret() public view onlyOwner returns (string memory) {
-        return SecretVault(secretVault).getSecret();
+        return SecretVault(secretVault).getSecret();  // Interaction
     }
 }
+
+/**
+ There is one more way for this called 'Dependency Injection', written in '01-Solidity.md' (Search SecretVault there in the Interacting with other smart contract section)
+ */
+
+
+
+/**
+
+License 
+
+// I can't write license here as it's written above
+
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    address owner;
+    string secret;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "must be owner");
+        _;
+    }
+
+    constructor(string memory _secret) {
+        secret = _secret;
+        owner = msg.sender;
+    }
+
+    function getSecret() public view onlyOwner returns (string memory) {
+        return secret;
+    }
+}
+
+ */
