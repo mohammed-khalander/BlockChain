b1dfaa5ec021691cf56d7648becbd48fc0ccb5e7
KhalanderCSE734 <khalandermohammed734@gmail.com>
2026-01-31 16:47:23 +0530

    HotelRoom Smart Contract

--- Basics/master_solidity/examples/6_HotelRoom.sol
+++ Basics/master_solidity/examples/6_HotelRoom.sol
@@ -3,6 +3,14 @@ pragma solidity ^0.8.0;
 
 // Showcase payment transfers, enums, modifiers & events
 
+
+/**
+  * Ether Payments
+  * Modifiers
+  * Visibility
+  * Events
+  * Enums
+ */
 contract HotelRoom {
     enum Statuses {
         Vacant,
@@ -11,8 +19,9 @@ contract HotelRoom {
     Statuses public currentStatus;
 
     event Occupy(address _occupant, uint256 _value);
+    // event Occupy(address indexed _occupant, uint256 _value, bytes data);
 
-    address payable public owner;
+    address payable public owner;   // 'Payable' allows this address to receive Ethereum CryptoCurrency
 
     constructor() {
         owner = payable(msg.sender);
@@ -30,11 +39,34 @@ contract HotelRoom {
     }
 
     function book() public payable onlyWhileVacant costs(2 ether) {
+        /**
+          * Here '2 ether' literally converts to actual correct amount.
+         */
+
         currentStatus = Statuses.Occupied;
 
+        // owner.transfer(msg.value)
+
         (bool sent, bytes memory data) = owner.call{value: msg.value}("");
-        require(sent);
+        require(sent, "Payment Failed");
+
 
         emit Occupy(msg.sender, msg.value);
+        // emit Occupy(msg.sender, msg.value, data);
     }
+
+    modifier onlyOwner(){
+        require(msg.sender==owner,"Only Owners are permitted to perform");
+        _;
+    }
+
+    function checkout() public onlyOwner {
+        currentStatus = Statuses.Vacant;
+    }
+
+
 }
+
+
+// Refer Basics/07-msg_keyword.md, 
+// Explanation for 'transfer' keyword is there
